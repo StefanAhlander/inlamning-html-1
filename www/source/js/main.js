c0a330a98fa80cf39c60696687b4b4f0f1387f1a
Stefan Åhlander <cs.ahlander@gmail.com>
2019-09-29 16:48:02 +0200

    Add IE and Edge detection

--- www/source/js/main.js
+++ www/source/js/main.js
@@ -1,3 +1,27 @@
-[].forEach.call( document.getElementsByClassName('preload'), function(element) {
+[].forEach.call(document.getElementsByClassName('preload'), function (element) {
     element.classList.remove("preload");
- });
+});
+
+function detectIE() {
+    let ua = window.navigator.userAgent;
+
+    let msie = ua.indexOf('MSIE ');
+    if (msie > 0) {
+        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    }
+
+    let trident = ua.indexOf('Trident/');
+    if (trident > 0) {
+        var rv = ua.indexOf('rv:');
+        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
+
+    var edge = ua.indexOf('Edge/');
+    if (edge > 0) {
+        return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+    }
+
+    return false;
+}
+
+if (detectIE()) document.body.classList.add("ie");
