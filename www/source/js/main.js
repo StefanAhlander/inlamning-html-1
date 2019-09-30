612ee9eb4ba7de0fe93780cd9627a2ecde76ff72
Stefan Åhlander <cs.ahlander@gmail.com>
2019-09-30 09:08:03 +0200

    Clean up in CSS and JS

--- www/source/js/main.js
+++ www/source/js/main.js
@@ -1,27 +1,9 @@
+/*  Remove .preload class from elements. The .preload class is used
+    to prevent CSS transforms fireing on page load. */
 [].forEach.call(document.getElementsByClassName('preload'), function (element) {
     element.classList.remove("preload");
 });
 
-function detectIE() {
-    let ua = window.navigator.userAgent;
-
-    let msie = ua.indexOf('MSIE ');
-    if (msie > 0) {
-        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
-    }
-
-    let trident = ua.indexOf('Trident/');
-    if (trident > 0) {
-        var rv = ua.indexOf('rv:');
-        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
-    }
-
-    var edge = ua.indexOf('Edge/');
-    if (edge > 0) {
-        return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
-    }
-
-    return false;
-}
-
-if (!detectIE()) document.body.classList.remove("ie");
+/*  Check if user agent is not IE or Edge. If so remove .ie class
+    from body element to permit parallax scrolling effect. */
+if (!/Edge|MSIE|Trident/i.test(navigator.userAgent)) document.body.classList.remove("ie");
