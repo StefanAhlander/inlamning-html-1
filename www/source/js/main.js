984d62ad1b2b48ca49af780ddbe29c6a1641de25
Stefan Åhlander <cs.ahlander@gmail.com>
2020-01-09 14:02:31 +0100

    Final commits

--- www/source/js/main.js
+++ www/source/js/main.js
@@ -1,9 +1,10 @@
 /*  Remove .preload class from elements. The .preload class is used
     to prevent CSS transforms fireing on page load. */
-[].forEach.call(document.getElementsByClassName('preload'), function (element) {
-    element.classList.remove("preload");
+[].forEach.call(document.getElementsByClassName("preload"), function (element) {
+  element.classList.remove("preload");
 });
 
 /*  Check if user agent is not IE or Edge. If so remove .ie class
     from body element to permit parallax scrolling effect. */
-if (!/Edge|MSIE|Trident/i.test(navigator.userAgent)) document.body.classList.remove("ie");
+if (!/Edge|MSIE|Trident/i.test(navigator.userAgent))
+  document.body.classList.remove("ie");
